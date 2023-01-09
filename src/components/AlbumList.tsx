cc05e9db0d2335d140679704976e4ae20f21dabe
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-01-09 12:37:33 +0900

    複数のSuspenseを組み合わせるためのコンポーネントを用意

---
+++ src/components/AlbumList.tsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+
+type Album = {
+  userId: number;
+  id: number;
+  title: string;
+};
+
+const fetchAlbums = async () => {
+  const result = await axios.get<Album[]>(
+    "https://jsonplaceholder.typicode.com/albums"
+  );
+  return result.data;
+};
+
+export const AlbumList = () => {
+  const { data } = useQuery<Album[]>(["albums"], fetchAlbums);
+  return (
+    <div
+      style={{
+        height: "300px",
+        border: "2px solid gray",
+        background: "cornsilk",
+        overflowY: "scroll",
+      }}
+    >
+      <h2>アルバム</h2>
+      <p>ReactQuery</p>
+      {data?.map((album) => (
+        <p key={album.id}>{album.title}</p>
+      ))}
+    </div>
+  );
+};
--- src/components/ReactQuery.tsx
+++ src/components/ReactQuery.tsx
@@ -1,31 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-
-type Album = {
-  userId: number;
-  id: number;
-  title: string;
-};
-
-const fetchAlbums = async () => {
-  const result = await axios.get<Album[]>(
-    "https://jsonplaceholder.typicode.com/albums"
-  );
-  return result.data;
-};
+import { AlbumList } from "./AlbumList";
+import { Sidebar } from "./Sidebar";
+import { TodoList } from "./TodoList";
 
 export const ReactQuery = () => {
-  const { data } = useQuery<Album[]>(["albums"], fetchAlbums);
-
-  // if (error) return <p>エラーです！</p>;
-  // if (isLoading) return <p>ローディング中です！</p>;
-
   return (
-    <div>
-      <p>ReactQuery</p>
-      {data?.map((album) => (
-        <p key={album.id}>{album.title}</p>
-      ))}
+    <div style={{ display: "flex", padding: "16px" }}>
+      <Sidebar />
+      <div style={{ flexGrow: 1 }}>
+        <AlbumList />
+        <TodoList />
+      </div>
     </div>
   );
 };
