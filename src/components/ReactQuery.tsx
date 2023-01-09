86d7d4499fd144437ce3cc9eeaa5ac46efc03b5c
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-01-09 13:02:03 +0900

    TransitionとSuspense組み合わせのための準備

--- src/components/ReactQuery.tsx
+++ src/components/ReactQuery.tsx
@@ -1,22 +1,55 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { AlbumList } from "./AlbumList";
 import { Sidebar } from "./Sidebar";
 import { TodoList } from "./TodoList";
 import { ErrorBoundary } from "react-error-boundary";
 
+type Tabs = "todo" | "album";
+
 export const ReactQuery = () => {
+  const [selectedTab, setSelectedTab] = useState<Tabs>("todo");
+
+  const buttonStyle = {
+    padding: "12px",
+    fontSize: "16px",
+    border: "none",
+  };
+
+  const albumButtonStyle = {
+    ...buttonStyle,
+    backgroundColor: selectedTab === "album" ? "royalblue" : "white",
+    color: selectedTab === "album" ? "white" : "black",
+  };
+
+  const todoButtonStyle = {
+    ...buttonStyle,
+    backgroundColor: selectedTab === "todo" ? "royalblue" : "white",
+    color: selectedTab === "todo" ? "white" : "black",
+  };
+
+  const onClickTabButton = (tab: Tabs) => {
+    setSelectedTab(tab);
+  };
+
   return (
     <div style={{ display: "flex", padding: "16px" }}>
       <Sidebar />
       <div style={{ flexGrow: 1 }}>
+        <button
+          style={todoButtonStyle}
+          onClick={() => onClickTabButton("todo")}
+        >
+          Todo
+        </button>
+        <button
+          style={albumButtonStyle}
+          onClick={() => onClickTabButton("album")}
+        >
+          Album
+        </button>
         <ErrorBoundary fallback={<p>AlbumListエラーです</p>}>
           <Suspense fallback={<p>AlbumListローディング中です</p>}>
-            <AlbumList />
-          </Suspense>
-        </ErrorBoundary>
-        <ErrorBoundary fallback={<p>TodoListエラーです</p>}>
-          <Suspense fallback={<p>TodoListローディング中です</p>}>
-            <TodoList />
+            {selectedTab === "todo" ? <TodoList /> : <AlbumList />}
           </Suspense>
         </ErrorBoundary>
       </div>
