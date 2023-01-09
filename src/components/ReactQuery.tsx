80c7af61ece9089be6bd45b0e4a484b755d8bdbe
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-01-09 10:44:07 +0900

    Suspenseを常にオンにするようReactQueryにデフォルトの設定を追加

--- src/components/ReactQuery.tsx
+++ src/components/ReactQuery.tsx
@@ -15,11 +15,7 @@ const fetchAlbums = async () => {
 };
 
 export const ReactQuery = () => {
-  const { isLoading, error, data } = useQuery<Album[]>(
-    ["albums"],
-    fetchAlbums,
-    { suspense: true }
-  );
+  const { isLoading, error, data } = useQuery<Album[]>(["albums"], fetchAlbums);
 
   // if (error) return <p>エラーです！</p>;
   // if (isLoading) return <p>ローディング中です！</p>;
