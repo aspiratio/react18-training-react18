c4289676e564cb181400082073e04d75b83be63d
YUHEI <80562226+aspiratio@users.noreply.github.com>
2023-01-09 12:43:14 +0900

    Suspenseをネスト

--- src/components/ReactQuery.tsx
+++ src/components/ReactQuery.tsx
@@ -1,14 +1,20 @@
+import { Suspense } from "react";
 import { AlbumList } from "./AlbumList";
 import { Sidebar } from "./Sidebar";
 import { TodoList } from "./TodoList";
+import { ErrorBoundary } from "react-error-boundary";
 
 export const ReactQuery = () => {
   return (
     <div style={{ display: "flex", padding: "16px" }}>
       <Sidebar />
       <div style={{ flexGrow: 1 }}>
-        <AlbumList />
-        <TodoList />
+        <ErrorBoundary fallback={<p>Listエラーです</p>}>
+          <Suspense fallback={<p>Listローディング中です</p>}>
+            <AlbumList />
+            <TodoList />
+          </Suspense>
+        </ErrorBoundary>
       </div>
     </div>
   );
